9a595b50874eb1af78579e1b5a9f97dad3566908
Florian Le Heurte <florian.leheurte@lecollectionist.com>
2021-07-13 11:09:32 +0800

    chore(LcInput): [ch7087] add unit tests and add storie

---
+++ src/components/LcInput/__tests__/LcInput.spec.ts
@@ -0,0 +1,54 @@
+import { mount } from '@vue/test-utils'
+import LcInput from '../LcInput'
+
+let wrapper: any
+
+beforeEach(() => {
+  wrapper = mount(LcInput, { props: { name: 'firstname' } })
+})
+
+afterEach(() => {
+  wrapper?.unmount()
+})
+
+describe('LcInput', () => {
+  it('is a Vue instance', () => {
+    expect(wrapper.vm).toBeTruthy()
+  })
+
+  it('should set good id and name attributes', () => {
+    const input = wrapper.find('input')
+
+    expect(input.attributes('id')).toBe('firstname')
+    expect(input.attributes('name')).toBe('firstname')
+  })
+
+  it('should render a label', async() => {
+    const labelText = 'Your Firstname'
+    await wrapper.setProps({ label: labelText })
+    const label = wrapper.get('label')
+
+    expect(label.text()).toBe(labelText)
+  })
+
+  it('should set the right input class', async() => {
+    await wrapper.setProps({ wrapperClass: 'foo' })
+
+    expect(wrapper.classes()).toContain('foo')
+  })
+
+  it('should set a placeholder', async() => {
+    const placeholderText = 'your firstname'
+    await wrapper.setProps({ placeholder: placeholderText })
+    const input = wrapper.find('input')
+
+    expect(input.attributes('placeholder')).toBe(placeholderText)
+  })
+
+  it('should emit on-blur event when leave input', async() => {
+    const input = wrapper.find('input')
+    await input.trigger('blur')
+
+    expect(wrapper.emitted('on-blur')).toBeTruthy()
+  })
+})
