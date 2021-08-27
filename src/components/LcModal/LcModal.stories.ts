4e05f37e55bc716c82e90d6d4ab66ce302622c61
Florian Le Heurte <florian.leheurte@lecollectionist.com>
2021-08-27 15:20:27 +0800

    feat(LcModal): add stories and v-model in stories

--- src/components/LcModal/LcModal.stories.ts
+++ src/components/LcModal/LcModal.stories.ts
@@ -4,6 +4,9 @@ import LcModal from './LcModal'
 export default {
   title: 'Example/LcModal',
   component: LcModal,
+  parameters: {
+    layout: 'centered',
+  },
 }
 
 const Template = (args: any) => ({
@@ -11,11 +14,15 @@ const Template = (args: any) => ({
   setup() {
     return { args }
   },
+  data() {
+    return { open: args.modelValue }
+  },
   template: `
-    <lc-modal v-bind="args">
+    <lc-button @click="open = !open">Open modal</lc-button>
+    <lc-modal v-bind="args" v-model="open">
       <template #content>Hello world</template>
       <template #footer>
-        <lc-button>Valider</lc-button>
+        <lc-button>Validate</lc-button>
       </template>
     </lc-modal>
   `,
@@ -23,6 +30,31 @@ const Template = (args: any) => ({
 
 export const Base = Template.bind({}) as any
 Base.args = {
-  modelValue: true,
+  modelValue: false,
   title: 'Hello',
+}
+
+export const OpenDefault = Template.bind({}) as any
+OpenDefault.args = {
+  ...Base.args,
+  modelValue: true,
+}
+
+export const WithoutHeader = Template.bind({}) as any
+WithoutHeader.args = {
+  ...Base.args,
+  noHeader: true,
+}
+
+export const WithoutFooter = Template.bind({}) as any
+WithoutFooter.args = {
+  ...Base.args,
+  noFooter: true,
+}
+
+export const WithoutBoth = Template.bind({}) as any
+WithoutBoth.args = {
+  ...Base.args,
+  noHeader: true,
+  noFooter: true,
 }
